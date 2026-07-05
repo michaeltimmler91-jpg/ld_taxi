d028790658223138e578a2d1d54adb8f15d5f765
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-05 12:34:15 +0200

    Add shared events

---
+++ resources/ld_taxi/shared/events.lua
@@ -0,0 +1,16 @@
+TaxiEvents = {
+    OrderCreated = 'order.created',
+    OrderAssigned = 'order.assigned',
+    OrderAccepted = 'order.accepted',
+    OrderCompleted = 'order.completed',
+    OrderReturned = 'order.returned',
+    OrderCancelled = 'order.cancelled',
+
+    DriverClockedIn = 'driver.clocked_in',
+    DriverClockedOut = 'driver.clocked_out',
+    DriverStatusChanged = 'driver.status_changed',
+
+    DispatchStarted = 'dispatch.started',
+    DispatchEnded = 'dispatch.ended',
+    DispatchHandover = 'dispatch.handover'
+}
