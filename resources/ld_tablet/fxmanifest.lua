234bbb67599c85174a2f60d0a2ea96c0744e7886
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-05 12:40:12 +0200

    Add ld_tablet manifest

---
+++ resources/ld_tablet/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Lennox'
+description 'LD Tablet Framework'
+version '0.1.0'
+
+lua54 'yes'
+
+client_scripts {
+    'client/main.lua'
+}
+
+ui_page 'web/index.html'
+
+files {
+    'web/index.html',
+    'web/style.css',
+    'web/app.js'
+}
