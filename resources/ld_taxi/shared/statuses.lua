11927e07134b9d5287546b5f0a795b3ea56b6d9b
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-05 12:33:39 +0200

    Add shared statuses

---
+++ resources/ld_taxi/shared/statuses.lua
@@ -0,0 +1,33 @@
+DriverStatus = {
+    Offline = 'offline',
+    Available = 'available',
+    Pause = 'pause',
+    Dispatch = 'dispatch',
+    Assigned = 'assigned',
+    EnRoute = 'en_route',
+    Arrived = 'arrived',
+    InRide = 'in_ride',
+    Delivery = 'delivery'
+}
+
+OrderStatus = {
+    New = 'new',
+    Dispatch = 'dispatch',
+    Assigned = 'assigned',
+    Accepted = 'accepted',
+    EnRoute = 'en_route',
+    Arrived = 'arrived',
+    Started = 'started',
+    Completed = 'completed',
+    Returned = 'returned',
+    Cancelled = 'cancelled'
+}
+
+OrderType = {
+    Person = 'person',
+    Food = 'food',
+    EMS = 'ems',
+    VehicleTransport = 'vehicle_transport',
+    Bambi = 'bambi',
+    Special = 'special'
+}
